1ef25fc64798a180236f1d86cd8b5066d354fc07
tukjw <yyb06001@tukorea.ac.kr>
2024-05-19 19:59:31 +0900

    일반, 백그라운드, 병렬모드 실행 구현

--- tush.c
+++ tush.c
@@ -6,6 +6,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/* strtok 함수 사용법 참고 : https://hackerpark.tistory.com/entry/C%EC%96%B8%EC%96%B4-strtok-%ED%95%A8%EC%88%98-%EB%AC%B8%EC%9E%90%EC%97%B4-%EC%9E%90%EB%A5%B4%EA%B8%B0 */
+
+
+/* 최대 글자 수, 최대 명령어 갯수 설정 */
 #define BUF_SIZE 400
 #define MAX_ARGS 15
 
@@ -21,13 +25,12 @@
 
 /* 테스트용 Args프린트하기 */
 void printArgs(char**args){
-    printf("====args====\n");
+    printf("args : [");
     int i;
     for(i=0;args[i]!=NULL;i++){
-        printf("[%d] : %s\n", i, args[i]);
+        printf("\"%s\", ", args[i]);
     }
-    printf("[%d] : NULL\n", i);
-    printf("=============\n");
+    printf("NULL]\n");
 }
 
 /* 모드 선택기 */
@@ -45,22 +48,101 @@ int modeSelector(char *input){
             ampersand_count++;
     }
 
-    printf("마지막 문자 : %c\n", input[i-1]);
     if (ampersand_count==0) return NORMAL_MODE;
     else if ((ampersand_count==1) && (input[i-1]=='&')) return BACKGROUND_MODE;
     else return PARALLEL_MODE;
 
 }
 
+/* args의 갯수 (NULL까지 몇개 있는지 확인하기 위함) */
+int getArgsCount(char **args){
+    int i = 0;
+    for(i=0;args[i]!=NULL;i++){}
+    return i;
+}
+
+/* 문자열의 마지막 문자 확인 */
+char getLastCharacter(char* str){
+    int i = 0;
+    for(i=0;str[i]!='\0';i++){}
+    return str[i-1];
+}
+
+/* input에서 speicla로 입력받은 문자열을 기준으로 쪼개서 매개변수로받은 commands에 저장함 */
+void specialSplit(char *input, char **commands, char *special){
+    char *token;
+    int command_count = 0;
+
+    token = strtok(input, special);
+    while (token != NULL) {
+        commands[command_count++] = token;
+        token = strtok(NULL, special);
+    }
+    commands[command_count] = NULL;
+}
+
+void process_run(char **args, int mode){
+        /* 프로세스 실행하는부분 */
+        pid_t pid = fork();
+
+        if (pid < 0){
+            fputs("fork 에러 발생\n", stderr);
+        }
+
+        else if (pid==0){ // 자식프로세스
+            execvp(args[0], args);
+            fputs("child process error\n", stderr);
+        }
+
+        else { // 부모 프로세스
+            if (mode==BACKGROUND_MODE) {
+                printf("child process PID : %d\n", pid);
+            }
+            else{
+                wait(NULL);
+            }
+        }
+}
+
+/* 내부 명령어인지 확인하고 내부명령어 일시 명령어를 실행 () */
+int internalCommand(char **args){
+    /* chdir 참고 : https://hackingis.art/30 */
+    if(!strcmp(args[0], "cd")){
+        if (chdir(args[1])!=0)
+            fputs("Failed change directory!\n", stderr);
+        return 1;
+    }
+    
+    else if(!strcmp(args[0], "exit")){
+        exit(1);
+    }
+
+    // TODO path 내장명령어 만들기
+    else if(!strcmp(args[0], "path")){
+        
+        return 1;
+    }
+
+    else
+        return 0;
+}
+
 int main(int argc, char *argv[]){
     char input[BUF_SIZE];
-    char command[BUF_SIZE];
-    char *args[MAX_ARGS];
+
+    char *commands[BUF_SIZE];
+    int command_count;
+
     char *token;
     char path[BUF_SIZE] = "/bin";
-    int args_count;
     int mode=0;
     int redirection_mode=0;
+
+    char lastCharacter;
+
+
+    int args_count;
+    char *args[MAX_ARGS];
 
     /* TODO 배치모드 (파일내용에 있는 명령어들 실행) */
     if (argc==2) {
@@ -72,90 +154,67 @@ int main(int argc, char *argv[]){
         printf("tush> ");
         fgets(input, sizeof(input), stdin);
         input[strlen(input)-1] = '\0';
-        printf("사용자가 입력한 명령어 : %s\n", input);
+        lastCharacter = getLastCharacter(input);
 
-        /* 모드 선택 */
+        /* 모드 리턴 (입력한 것에 따라서) */
         mode = modeSelector(input);
 
-        /* 백그라운드 모드이면 문자열 맨끝 &기호를 제거 */
-        if (mode==BACKGROUND_MODE){
-            input[strlen(input)-1]=0;
-            printf("%s\n", input);
-        }
-        printf("MODE : %d\n", mode);
-
-        /* 스페이스 기준으로 문자열을 나눠서 args 포인터 배열에 저장 (배열 마지막은 NULL) */
-        /* 참고 : https://hackerpark.tistory.com/entry/C%EC%96%B8%EC%96%B4-strtok-%ED%95%A8%EC%88%98-%EB%AC%B8%EC%9E%90%EC%97%B4-%EC%9E%90%EB%A5%B4%EA%B8%B0 */
-        args_count = 0;
-        token = strtok(input, " ");
-        while(token != NULL){
-            args[args_count++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[args_count] = NULL;
-
-        printArgs(args);
-
-        /* 내부 명령어 */
-        /* chdir 참고 : https://hackingis.art/30 */
-        if(!strcmp(args[0], "cd")){
-            if (chdir(args[1])!=0)
-                fputs("Failed change directory!\n", stderr);
-            continue;
-        }
+        specialSplit(input, commands, "&");
         
-        else if(!strcmp(args[0], "exit")){
-            exit(1);
-        }
+        /* Args 확인 */
+        printf("command ");
+        printArgs(commands);
 
-        /* TODO path 구현하기 */
-        else if(!strcmp(args[0], "path")){
-            
-        }
+        /*&기호로*/
+        command_count = getArgsCount(commands);
 
-        /* 프로세스 실행하는부분 */
-        pid_t pid = fork();
+        /* 명령어가 단일일 경우 (중간에 &기호가 없는경우) */
+        if ( (command_count==1) && (lastCharacter!='&')){
+            printf("case 1 ");
+            specialSplit(commands[0], args, " ");
+            printArgs(args);
 
-        if (pid < 0){
-            fputs("fork 에러 발생\n", stderr);
-            continue;
-        }
-
-        else if (pid==0){ // 자식프로세스
-            
-            if (mode==REDIRECTION_MODE){ // 리다이렉션 모드 일때
-
-                switch(redirection_mode){
-
-                case REDIRECTION_OUTPUT: // cmd > filename
-                    close(STDOUT_FILENO);
-                    open("test.txt", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-                    break;
-
-                case REDIRECTION_APPEND: // cmd >> filename
-                    close(STDOUT_FILENO);
-                    open("test.txt", O_CREAT|O_WRONLY|O_APPEND, S_IRWXU);
-                    break;
-
-                case REDIRECTION_INPUT:    //TODO cmd < filename
-
-                default:
-                    break;
-                }   
-            }
-            execvp(args[0], args);
-            fputs("child 프로세스 error\n", stderr);
-        }
-
-        else { // 부모 프로세스
-            if (mode==BACKGROUND_MODE) {
-                printf("자식 프로세스 PID : %d\n", pid);
-                continue;
-            }
-            else{
-                wait(NULL);
+            /* 내장 명령어인지 확인겸 실행하고, 아닐시 외부 명령어 실행*/
+            if (!internalCommand(args)){
+                process_run(args, NORMAL_MODE);
             }
         }
+
+        /* 명령어가 단일이며 &로 끝나는 경우 */
+        else if ( (command_count==1) && (lastCharacter=='&')){
+            printf("case 2 ");
+            specialSplit(commands[0], args, " ");
+            printArgs(args);
+            process_run(args, BACKGROUND_MODE);
+        }
+
+        /* command_count가 2 이상이며, input의 마지막 문자는 &기호가 아닌 경우 */
+        else if ((command_count>=2) && (lastCharacter!='&')){
+            for (int i = 0; i < command_count; i++) {
+                specialSplit(commands[i], args, " ");
+                printf("case 3 ");
+                printArgs(args);
+
+                /* 마지막 명령어 이전까지는 BACKGROUND로 실행, 마지막 명령어는 일반으로 실행*/
+                if (i<command_count-1)
+                    process_run(args, BACKGROUND_MODE);
+                else
+                    process_run(args, NORMAL_MODE);
+            }
+        }
+
+        /* command_count가 2 이상이며, input의 마지막 문자는 &기호 일 경우 */
+        /* 모든 명령어를 백그라운드로 실행 */
+        else if ((command_count>=2) && (lastCharacter=='&')){
+            for (int i = 0; i < command_count; i++) {
+                specialSplit(commands[i], args, " ");
+                printf("case 4 ");
+                printArgs(args);
+                process_run(args, BACKGROUND_MODE);
+            }
+        }
+
+
     }
 
     return 0;
