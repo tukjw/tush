564464946d743c429c4a23922dd1d19001c12282
tukjw <yyb06001@tukorea.ac.kr>
2024-05-22 12:29:35 +0900

    공백제거 함수추가

--- tush.c
+++ tush.c
@@ -17,9 +17,9 @@
 #define REDIRECTION_MODE 4
 #define PARALLEL_MODE 5
 
-#define REDIRECTION_INPUT 0
-#define REDIRECTION_OUTPUT 1
-#define REDIRECTION_APPEND 2
+#define REDIRECTION_INPUT 1 
+#define REDIRECTION_OUTPUT 2
+#define REDIRECTION_APPEND 3
 
 /* 테스트용 Args프린트하기 */
 void printArgs(char**args){
@@ -31,15 +31,7 @@ void printArgs(char**args){
     printf("NULL]\n");
 }
 
-/* 리다이렉션 모드인지 확인 */
-int isRedirectionMode(char *input){
-    int i;
-    for(i=0;input[i]!='\0';i++){
-        if(input[i]=='<' || input[i]=='>')
-            return 1;
-    }
-    return 0;
-}
+
 
 /* args의 갯수 (NULL까지 몇개 있는지 확인하기 위함) */
 int getArgsCount(char **args){
@@ -53,6 +45,20 @@ char getLastCharacter(char* str){
     int i = 0;
     for(i=0;str[i]!='\0';i++){}
     return str[i-1];
+}
+
+/* 문자열 공백 제거 함수 */
+/* 참고 : https://m.blog.naver.com/coding-abc/221781138470 , 수정*/
+char* DeleteSpace(char *s)
+{
+    char* str = (char*)malloc(strlen(s) + 1);
+    int i, k = 0;
+
+    for (i = 0; i < strlen(s); i++)
+        if (s[i] != ' ') str[k++] = s[i];
+
+    str[k] = '\0';
+    return str;
 }
 
 /* input에서 speicla로 입력받은 문자열을 기준으로 쪼개서 매개변수로받은 commands에 저장함 */
@@ -95,6 +101,52 @@ void process_run(char **args, int mode){
         }
 }
 
+/* 리다이렉션 모드인지 확인 */
+int isRedirectionMode(char *input){
+    int i;
+    for(i=0;input[i]!='\0';i++){
+        if(input[i]=='<')
+			return REDIRECTION_INPUT;
+		if(input[i]=='>')
+			if(input[i+1]=='>')
+				return REDIRECTION_APPEND;
+			else
+				return REDIRECTION_OUTPUT;
+
+    }
+    return 0;
+}
+
+/* 리다이렉션 실행 */
+/* 구현중 */
+void redirectionProcessrun(char *input, int redirection_mode, int run_mode){
+	char *args[MAX_ARGS];
+	printf("리다이렉션 모드 실행\n");
+	
+	if (redirection_mode==REDIRECTION_OUTPUT){
+		printf("output\n");
+		specialSplit(input, args, ">");
+		printArgs(args);
+		process_run(args, run_mode);
+	}
+
+	else if (redirection_mode==REDIRECTION_APPEND){
+		printf("append\n");
+		specialSplit(input, args, ">");
+		printArgs(args);
+	}
+	
+
+	/* TODO 리다이렉션 input 구현해야함 */
+	else if (redirection_mode==REDIRECTION_INPUT){
+		printf("input\n");
+	}
+
+	printArgs(args);
+}
+
+
+
 /* 내부 명령어인지 확인하고 내부명령어 일시 명령어를 실행 () */
 int internalCommand(char **args){
     /* chdir 참고 : https://hackingis.art/30 */
@@ -119,9 +171,8 @@ int internalCommand(char **args){
 }
 
 void batchMode(char **argv){
-    // batch mode 변수
     int fd;
-    char test[BUF_SIZE];
+    char input[BUF_SIZE];
     int str_len;
     char *commands[BUF_SIZE];
     int command_count;
@@ -135,9 +186,9 @@ void batchMode(char **argv){
         exit(1);
     }
 
-    str_len = read(fd, test, sizeof(test));
-    test[str_len]=0;
-    specialSplit(test, commands, "\n");
+    str_len = read(fd, input, sizeof(input));
+    input[str_len]=0;
+    specialSplit(input, commands, "\n");
     command_count = getArgsCount(commands);
 
     for(i=0;commands[i]!=NULL;i++){
@@ -160,10 +211,10 @@ int main(int argc, char *argv[]){
     char *args[MAX_ARGS];
     int i;
 
-    // batch mode 변수
-    int fd;
-    char test[BUF_SIZE];
-    int str_len;
+    // TODO
+    // char test[BUF_SIZE] = "가      나   다   라 마 바 사";
+    // a
+    // DeleteSpace(test);
 
     /* 배치모드 (파일내용에 있는 명령어들 실행) */
     if (argc==2) {
@@ -183,9 +234,6 @@ int main(int argc, char *argv[]){
         input[strlen(input)-1] = '\0';
         lastCharacter = getLastCharacter(input);
 
-        /* TODO 리다이렉션 모드인지 확인 */
-        mode = isRedirectionMode(input);
-
         /* 사용자의 명령어를 &기호로 나눔 */
         specialSplit(input, commands, "&");
         
@@ -198,15 +246,24 @@ int main(int argc, char *argv[]){
 
         /* 명령어가 단일일 경우 (중간에 &기호가 없는경우) */
         /* 내장명령어, 리다이렉션 모두 단일 명령어에서 실행 */
-        if ( (command_count==1) && (lastCharacter!='&')){
-            specialSplit(commands[0], args, " ");
+		if ( (command_count==1) && (lastCharacter!='&')){
 
-            /* 내장 명령어인지 확인겸 실행하고, 아닐시 외부 명령어 실행*/
-            if (!internalCommand(args)){
-                process_run(args, NORMAL_MODE);
-            }
+			/* 리다이렉션 모드일경우 */
+			if ((redirection_mode=isRedirectionMode(commands[0]))){
+				redirectionProcessrun(commands[0], redirection_mode, NORMAL_MODE);
+			}
 
-        }
+			/* 리다이렉션이 아닐경우 */
+			else{
+				specialSplit(commands[0], args, " ");
+				/* 명령어가 내장 명령어인지 확인겸 실행하고, 아닐시 외부 명령어 실행*/
+				if (!internalCommand(args))
+					process_run(args, NORMAL_MODE);
+			}
+
+
+
+		}
 
         /* 명령어가 단일이며 &로 끝나는 경우 */
         else if ( (command_count==1) && (lastCharacter=='&')){
