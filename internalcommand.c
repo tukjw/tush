b3f1241a4125299716bc130a9a1a0d8b1113795e
tukjw <yyb06001@tukorea.ac.kr>
2024-06-06 11:14:09 +0900

    인자없이 cd 입력시 홈 디렉토리로 가도록 업데이트

--- internalcommand.c
+++ internalcommand.c
@@ -14,14 +14,25 @@ int isInternalMode(char **args){
 
 /* 내장명령어 실행 */
 void internalCommandRun(char **args){
+
     /* chdir 참고 : https://hackingis.art/30 */
     if(!strcmp(args[0], "cd")){
-        if (chdir(args[1])!=0)
-            fputs("Failed change directory!\n", stderr);
-    }
-    
-    else if(!strcmp(args[0], "exit")){
-        exit(0);
+        /* 인자가 없거나 ~(틸드) 일 경우 "HOME" 환경변수를 읽어와 홈 디렉토리로 이동 */
+        if (args[1] == NULL || !strcmp(args[1], "~")) {
+            char *home = getenv("HOME");
+            if (home != NULL) {
+                if (chdir(home) != 0)
+                    fputs("Failed to change directory to HOME!\n", stderr);
+            } else {
+                fputs("HOME environment variable not set!\n", stderr);
+            }
+        } 
+
+        /* 인자가 있는 경우 해당 디렉토리로 이동 */
+        else {
+            if (chdir(args[1]) != 0)
+                fputs("Failed to change directory!\n", stderr);
+        }
     }
 
     // path
